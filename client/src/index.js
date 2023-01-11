cc61301dc4e043c27d8bd90afdfcc6dda5f3af3a
Pirin <chuckdanorris123@gmail.com>
2023-01-10 23:32:56 -0400

    added user query

--- client/src/index.js
+++ client/src/index.js
@@ -13,7 +13,7 @@ const store = configureStore ({
   
 })
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -8,13 +8,12 @@ export const globalSlice = createSlice({
     name: "global",
     initialState,
     reducers: {
-        setMode: (state) => {
-            state.mode = state.mode === 'light' ? "dark" : 'light';
-        }
-    }
-});
-
-export const { setMode } = globalSlice.actions;
-
-export default globalSlice.reducer;
-
+      setMode: (state) => {
+        state.mode = state.mode === "light" ? "dark" : "light";
+      },
+    },
+  });
+  
+  export const { setMode } = globalSlice.actions;
+  
+  export default globalSlice.reducer;
