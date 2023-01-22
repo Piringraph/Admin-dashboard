4e30ac0785b91ec8dcd3afe6ae0d8c161daaf632
Pirin <chuckdanorris123@gmail.com>
2023-01-21 23:48:58 -0400

    added API structure

--- server/data/index.js
+++ server/data/index.js
@@ -18041,7 +18041,7 @@ export const dataAffiliateStat = [
     },
     {
       _id: "63701cc1f03239b7f700000e",
-      name: "Shelly",
+      name: "Steve",
       email: "swelbeck12@ycombinator.com",
       password: "RSjzmAjnq",
       city: "Sangoleng",
